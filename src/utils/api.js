75c39a5cdd4aa5530feb172eccbb9b20f7440fad
Shekhar <mauryashekhar9837@gmail.com>
2024-06-23 17:48:20 +0530

    ...

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "https://youtube138.p.rapidapi.com";
-
+const apiUrl = process.env.REACT_APP_API_KEY;
 
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": 'process.env.REACT_API_KEY',
+    "X-RapidAPI-Key": apiUrl,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
