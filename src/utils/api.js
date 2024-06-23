01053cae1bd47428f2b1f974fcb5f7fd7b079b1e
Shekhar <mauryashekhar9837@gmail.com>
2024-06-23 17:51:01 +0530

    updated api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const apiUrl = process.env.REACT_APP_API_KEY;
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": apiUrl,
+    "X-RapidAPI-Key": '83ff89b8c7mshf33efdc1063b134p192ed6jsn15b04ed79f9a',
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
