93ff94e31328b9f030aa803490b7e9e2570bdc22
Shekhar <mauryashekhar9837@gmail.com>
2024-06-23 17:34:41 +0530

    ...

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_API_KEY,
+    "X-RapidAPI-Key": 'process.env.REACT_API_KEY',
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
